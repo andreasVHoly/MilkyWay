2f126537c19a5a8254a4fc9cfe39ce8a58d4c6f3
Andreas <andreas@flatcircle.io>
2021-07-11 14:15:43 +0200

    some documentation for bundle method

--- MilkyWay/Extensions/Bundle+getValue.swift
+++ MilkyWay/Extensions/Bundle+getValue.swift
@@ -5,11 +5,17 @@ enum BundleKey: String {
 }
 
 extension Bundle {
+    /**
+        Accesses values in the app's Bundle with the specified `BundleKey`
+     - Parameter key: The key to be looked up
+     - Returns: The Value stored under the key
+     - Warning: Be sure this value exists in your Bundle, as a missing value can cause a crash
+     */
     func getValue(for key: BundleKey) -> String {
         getValue(for: key.rawValue)
     }
 
-    public func getValue(for key: String) -> String {
+    private func getValue(for key: String) -> String {
         // swiftlint:disable:next force_cast
         object(forInfoDictionaryKey: key) as! String
     }
