650f549210af16ee311d63dffa61ca42950b72fe
Andreas <andreas@flatcircle.io>
2021-07-12 17:23:34 +0200

    basic empty state cherry pick

--- MilkyWay/Network/NetworkError.swift
+++ MilkyWay/Network/NetworkError.swift
@@ -7,4 +7,31 @@ enum NetworkError: Error {
     case networkError(Error?)
     case noResponse
     case parsingError(String)
+    case noResults
+
+    var errorDescription: String {
+        switch self {
+        case .networkError:
+            return "Network problem. Please check your internet connection and try again"
+        case .noResults:
+            return "Looks like there were no results. Please try again later."
+        default:
+            return "Looks like something went wrong. The team will look into this ASAP."
+        }
+    }
+}
+
+extension NetworkError: Equatable {
+    static func == (lhs: NetworkError, rhs: NetworkError) -> Bool {
+        switch (lhs, rhs) {
+        case (.invalidURL, .invalidURL): return true
+        case (.authenticationError, .authenticationError): return true
+        case (.serverError, .serverError): return true
+        case (.noResponse, .noResponse): return true
+        case (.noResults, .noResults): return true
+        case (.networkError(let lhsError), .networkError(let rhsError)): return lhsError?.localizedDescription == rhsError?.localizedDescription
+        case (.parsingError(let lhsError), .parsingError(let rhsError)): return lhsError == rhsError
+        default: return false
+        }
+    }
 }
