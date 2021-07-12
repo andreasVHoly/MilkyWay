197389a3cd05bc3732cef9f1f71c7fa8d200fb73
Andreas <andreas@flatcircle.io>
2021-07-12 10:29:02 +0200

    image adjustments and removed merged cache layers

--- MilkyWay/Image Loading/ImageCache.swift
+++ MilkyWay/Image Loading/ImageCache.swift
@@ -13,10 +13,6 @@ class ImageCache: ImageCachable {
     private lazy var imageCache: NSCache<AnyObject, AnyObject> = {
         let cache = NSCache<AnyObject, AnyObject>()
         cache.countLimit = itemLimit
-        return cache
-    }()
-    private lazy var decodedImageCache: NSCache<AnyObject, AnyObject> = {
-        let cache = NSCache<AnyObject, AnyObject>()
         cache.totalCostLimit = memoryLimit
         return cache
     }()
@@ -30,57 +26,21 @@ class ImageCache: ImageCachable {
 
     func retrieveImage(for url: URL) -> UIImage? {
         lock.lock(); defer { lock.unlock() }
-        if let decodedImage = decodedImageCache.object(forKey: url as AnyObject) as? UIImage {
-            return decodedImage
-        }
-        if let image = imageCache.object(forKey: url as AnyObject) as? UIImage {
-            let decodedImage = image.decodedImage()
-            decodedImageCache.setObject(image as AnyObject, forKey: url as AnyObject, cost: decodedImage.diskSize)
-            return decodedImage
-        }
-        return nil
+        return imageCache.object(forKey: url as AnyObject) as? UIImage
     }
 
     func store(image: UIImage, for url: URL) {
-        let decodedImage = image.decodedImage()
         lock.lock(); defer { lock.unlock() }
-        imageCache.setObject(decodedImage, forKey: url as AnyObject)
-        decodedImageCache.setObject(image as AnyObject, forKey: url as AnyObject, cost: decodedImage.diskSize)
+        imageCache.setObject(image, forKey: url as AnyObject)
     }
 
     func deleteImage(for url: URL) {
         lock.lock(); defer { lock.unlock() }
         imageCache.removeObject(forKey: url as AnyObject)
-        decodedImageCache.removeObject(forKey: url as AnyObject)
     }
 
     func clear() {
         lock.lock(); defer { lock.unlock() }
         imageCache.removeAllObjects()
-        decodedImageCache.removeAllObjects()
-    }
-}
-
-fileprivate extension UIImage {
-
-    func decodedImage() -> UIImage {
-        guard let cgImage = cgImage else { return self }
-        let size = CGSize(width: cgImage.width, height: cgImage.height)
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let context = CGContext(data: nil,
-                                width: Int(size.width),
-                                height: Int(size.height),
-                                bitsPerComponent: 8,
-                                bytesPerRow: cgImage.bytesPerRow,
-                                space: colorSpace,
-                                bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-        context?.draw(cgImage, in: CGRect(origin: .zero, size: size))
-        guard let decodedImage = context?.makeImage() else { return self }
-        return UIImage(cgImage: decodedImage)
-    }
-
-    var diskSize: Int {
-        guard let cgImage = cgImage else { return 0 }
-        return cgImage.bytesPerRow * cgImage.height
     }
 }
